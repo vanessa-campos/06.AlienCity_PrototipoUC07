718784551fdb45f8083d4637284bc85c8536d0e3
vanessa-campos <vanessa-campos@users.noreply.github.com>
2021-02-26 08:40:17 -0300

    começo atv 01 - apagar

---
+++ Assets/Scripts/PlayerTouchController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTouchController : MonoBehaviour
+{
+    [SerializeField] float Speed = 1;
+    [SerializeField] Text pontosText;
+    [SerializeField] Text jogoText;
+    Rigidbody2D _rigidibody;
+    bool touching = false;
+    private int pontos;
+    public int Pontos { get { return pontos; } set { pontos = value; pontosText.text = "PONTUAÇÃO: " + pontos; } }
+
+    void Start()
+    {
+        _rigidibody = GetComponent<Rigidbody2D>();
+        Pontos = 0;
+    }
+
+    void Update()
+    {
+
+        // Mostrar o texto quando o jogador estiver tocando na tela
+        if (touching)
+        {
+            jogoText.text = "Capture todas as gemas";
+        }
+        else
+        {
+            jogoText.text = "";
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Speed;
+        float vertical = Input.GetAxis("Vertical") * Speed;
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            touching = true;
+        }else{
+            touching = false;
+        }
+
+        //transform.Translate (horizontal, vertical, 0);
+        _rigidibody.velocity = new Vector3(horizontal * Speed, vertical * Speed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("gema"))
+        {
+            Pontos += 10;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnGems.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGems : MonoBehaviour
+{
+    [SerializeField] GameObject Gema;
+    [SerializeField] Transform[] SpawnPontos = new Transform[6];
+    GameObject newGema;
+
+    void Start()
+    {
+        RandomSpawn();RandomSpawn();RandomSpawn();       
+    }
+
+    void Update()
+    {
+        if(GameObject.FindGameObjectsWithTag("gema").Length != 3)
+        {
+            RandomSpawn();
+        }
+        foreach (var item in GameObject.FindGameObjectsWithTag("gema"))
+        {
+            if(item.transform.position == newGema.transform.position)
+            {
+                RandomSpawn();
+            }
+        }        
+    }
+
+    void RandomSpawn()    
+    {
+        newGema = Instantiate(Gema, SpawnPontos[Random.Range(0, 6)].position, Quaternion.identity);
+        Destroy(newGema, Random.Range(3,7));           
+    }
+
+    /*
+    void RandomSpawn()
+    {
+        //Sorteia um número para selecionar um dos pontos de spawn e instanciar uma gema nele
+        int p1 = Random.Range(0, 6);
+        Vector3 SpawnPosition = SpawnPontos[p1].position;
+        GameObject newGema = Instantiate(Gema, SpawnPosition, Quaternion.identity);
+        Destroy(newGema, Random.Range(3,7));
+
+        //Sorteia um novo número e verifica se não é repetido para então selecionar o respectivo ponto de spawn e instanciar outra gema nele        
+        int p2 = Random.Range(0, 6);
+        do
+        {
+            p2 = Random.Range(0, 6);
+        } while (p2 == p1);
+        SpawnPosition = SpawnPontos[p2].position;
+        newGema = Instantiate(Gema, SpawnPosition, Quaternion.identity);
+        Destroy(newGema, Random.Range(3,7));
+        int p3 = Random.Range(0, 6);
+        do
+        {
+            p3 = Random.Range(0, 6);
+        } while (p3 == p1 | p3 == p2);
+        SpawnPosition = SpawnPontos[p3].position;
+        newGema = Instantiate(Gema, SpawnPosition, Quaternion.identity);
+        Destroy(newGema, Random.Range(3,7));
+    }
+    */
+}
